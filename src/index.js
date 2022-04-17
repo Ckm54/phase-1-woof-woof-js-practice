1ac0ce200438d89df1dd79c03cc2a7254fbde3b1
Collins <mutaic84@gmail.com>
2022-04-17 11:16:08 +0300

    display dog data on header section

---
+++ src/index.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const header = document.getElementById("dog-bar")
+
+    fetch("http://localhost:3000/pups")
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(element => {
+            loadHeader(element)
+        });
+    })
+
+    function loadHeader(data) {
+        let span = document.createElement("span")
+        span.innerText = data.name
+        header.append(span)
+    }
+})
